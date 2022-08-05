11376347222234c13d89e94cfa16465b49348039
Remedystorm <sourignavong.ryan@gmail.com>
2022-08-05 11:50:27 -0500

    additional and final changes to CA

--- todo.js
+++ todo.js
@@ -6,10 +6,11 @@ document.getElementById('add').addEventListener('click', () => {
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
     row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() +1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
+    row.insertCell(1).innerHTML = document.getElementById('new-task2').value;
+    row.insertCell(2).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() +1}-${createdDate.getDate()}`;
     row.insertCell(3).innerHTML = document.getElementById('new-start-date').value;
-    let actions = row.insertCell(4);
+    row.insertCell(4).innerHTML = document.getElementById('new-start-date').value;
+    let actions = row.insertCell(5);
     actions.appendChild(createDeleteButton(id++));
     document.getElementById('new-task').value = '';
 
